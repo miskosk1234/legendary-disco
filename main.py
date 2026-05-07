0796505f4bc4ad4bf6c972638dc23cdc72285fa3
miskosk1234 <michal.hujo13@gmail.com>
2026-05-07 18:37:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-    print("ahoj github!)
+import random
+tajne_cislo = random.randint(1, 5)
+tip = int(input("Uhádni číslo od 1 do 5:"))
+if tip == tajne_cislo:
+    print("dobreeeee win")
+else:
+    print("game over skus to znova. Správne čislo bolo:", tajne_cislo)
